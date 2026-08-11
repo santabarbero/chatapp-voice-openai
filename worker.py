d52d4493387dd35add2652dd9f6168b0efffcd21
TuUsuarioDeGitHub <elvitoatb@gmail.com>
2026-08-10 23:04:57 -0400

    Guardando avances: Paso 4 completado

--- worker.py
+++ worker.py
@@ -3,9 +3,34 @@ import requests
 
 openai_client = OpenAI()
 
-
+    
 def speech_to_text(audio_binary):
-    return None
+
+	# Set up Watson Speech-to-Text HTTP Api url
+	base_url = 'base_url = "https://sn-watson-stt.labs.skills.network"
+'
+	api_url = base_url+'/speech-to-text/api/v1/recognize'
+
+	# Set up parameters for our HTTP request
+    params = {
+        'model': 'es-LA_Telephony',  # O bien 'es-ES_Multimedia' si está disponible
+    }
+	
+
+	# Set up the body of our HTTP request
+	body = audio_binary
+
+	# Send a HTTP Post request
+	response = requests.post(api_url, params=params, data=audio_binary).json()
+
+	# Parse the response to get our transcribed text
+	text = 'null'
+	while bool(response.get('results')):
+		print('speech to text response:', response)
+		text = response.get('results').pop().get('alternatives').pop().get('transcript')
+		print('recognised text: ', text)
+		return text
+
 
 
 def text_to_speech(text, voice=""):
